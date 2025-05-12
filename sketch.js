4a7724ef4a2e61d783aa9174f70e3d951613cb72
stella0808 <chichihsu95@gmail.com>
2025-05-12 10:39:01 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,7 +22,21 @@ function modelReady() {
 }
 
 function draw() {
-  image(video, 0, 0, width, height);
+  // 計算影像的繪製比例
+  let videoAspect = video.width / video.height;
+  let canvasAspect = width / height;
+
+  let drawWidth, drawHeight;
+  if (videoAspect > canvasAspect) {
+    drawWidth = width;
+    drawHeight = width / videoAspect;
+  } else {
+    drawHeight = height;
+    drawWidth = height * videoAspect;
+  }
+
+  // 繪製影像，保持比例
+  image(video, 0, 0, drawWidth, drawHeight);
 
   if (predictions.length > 0) {
     let keypoints = predictions[0].scaledMesh;
@@ -40,6 +54,11 @@ function draw() {
     for (let i = 0; i < points.length; i++) {
       let index = points[i];
       let [x, y] = keypoints[index];
+
+      // 映射特徵點到畫布大小
+      x = map(x, 0, video.width, 0, drawWidth);
+      y = map(y, 0, video.height, 0, drawHeight);
+
       vertex(x, y);
     }
     endShape(CLOSE); // 將最後一點與第一點連接
