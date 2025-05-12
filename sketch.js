cca8af0e509a2f1886592426fa30e9e2d479e1c1
stella0808 <chichihsu95@gmail.com>
2025-05-12 10:34:28 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,26 +22,37 @@ function modelReady() {
 }
 
 function draw() {
-  image(video, 0, 0, width, height);
+  // 計算比例，讓影像適應畫布而不變形
+  let videoAspect = video.width / video.height;
+  let canvasAspect = width / height;
+
+  let drawWidth, drawHeight;
+  if (videoAspect > canvasAspect) {
+    drawWidth = width;
+    drawHeight = width / videoAspect;
+  } else {
+    drawHeight = height;
+    drawWidth = height * videoAspect;
+  }
+
+  // 繪製影像，保持比例
+  image(video, 0, 0, drawWidth, drawHeight);
 
   if (predictions.length > 0) {
     let keypoints = predictions[0].scaledMesh;
 
-    // 指定的點的編號
-    let points = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+    let points = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291];
 
-    // 設定線條樣式
-    stroke(0, 0, 255); // 藍色
+    stroke(0, 0, 255);
     strokeWeight(5);
     noFill();
 
-    // 畫線將所有點串接在一起
     beginShape();
     for (let i = 0; i < points.length; i++) {
       let index = points[i];
       let [x, y] = keypoints[index];
       vertex(x, y);
     }
-    endShape(CLOSE); // 將最後一點與第一點連接
+    endShape(CLOSE);
   }
 }
